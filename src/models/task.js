f1fe03ac830f7002daf935841aa41e68d6e3e729
Arthur Ryzhyy <rizhiy2406@gmail.com>
2020-01-26 13:34:37 +0200

    Init commit

---
+++ src/models/task.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+//Creating task schema
+const taskSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  }
+}, {
+  timestamps: true
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
