bbf40b357279e563b0142da5d14ef93fb2ca7412
Arthur Ryzhyy <rizhiy2406@gmail.com>
2020-03-23 14:19:25 +0200

    npm audit fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 'use strict';
 
-const express = require('express');
-require('./db/mongoose'); // we did so because we only need to ensure that the file runs
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
+const app = require('./app');
 
 const port = process.env.PORT;
-const app = express();
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
